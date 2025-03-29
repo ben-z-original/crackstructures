0feae00dd3d3e03b0e9d7164865abba70bdf9a58
Christian Benz <christian.benz@uni-weimar.de>
2025-03-29 15:08:22 +0100

    Add result_dirname and tex_path parameter

--- src/crackstructures/evaluation/run.py
+++ src/crackstructures/evaluation/run.py
@@ -12,8 +12,11 @@ if __name__ == "__main__":
     parser.add_argument('--damage', nargs='+', default="crack",
                         help="Structural damage (or list of structural damages, in case they form a combined class")
     parser.add_argument('--vis', action='store_true', help="Enable visualization")
+    parser.add_argument('--result_dirname', type=str, default="out",
+                        help="Name of the results directory for predicted damage files")
+    parser.add_argument('--tex_path', type=Path, help="Path to save the tex output")
     args = parser.parse_args()
 
     segment_path = args.datadir / args.structure / args.segment
 
-    evaluate(segment_path, args.damage, vis=args.vis, tex_path=None)
+    evaluate(segment_path, args.damage, vis=args.vis, tex_path=args.tex_path, result_dirname=args.result_dirname)
